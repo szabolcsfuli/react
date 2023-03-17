3f3cff0064cd9979218cb361abff9f302def7894
szabolcsfuli <szabolcsfuli.dev@gmail.com>
2023-03-17 08:07:41 +0100

    total price fix

--- src/components/Cart/MiniCart.tsx
+++ src/components/Cart/MiniCart.tsx
@@ -4,7 +4,7 @@ import CartContext from "../../store/CartContext";
 
 const MiniCart = ({ navigation }: any) => {
     const ctx = useContext(CartContext);
-    const title = ctx.totalCount ? `${ctx.totalItems} Products, total price ${ctx.totalAmount.toFixed(2)}$` : 'No products in cart';
+    const title = ctx.totalCount ? `${ctx.totalItems} Products, total price $${ctx.totalAmount.toFixed(2)}` : 'No products in cart';
 
     return (
         <View>
