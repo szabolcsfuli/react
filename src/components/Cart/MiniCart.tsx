dafcbb5675bd3a334e3bd91b16510f2cdca1a747
szabolcsfuli <szabolcsfuli.dev@gmail.com>
2023-03-20 07:53:27 +0100

    mobile app

--- src/components/Cart/MiniCart.tsx
+++ src/components/Cart/MiniCart.tsx
@@ -1,16 +1,19 @@
-import { useContext } from "react"
-import { View, Button } from "react-native"
-import CartContext from "../../store/CartContext";
+import {useContext} from 'react'
+import {View, Text, Button} from 'react-native'
+import CartContext from '../../store/CartContext'
 
-const MiniCart = ({ navigation }: any) => {
-    const ctx = useContext(CartContext);
-    const title = ctx.totalCount ? `${ctx.totalItems} Products, total price $${ctx.totalAmount.toFixed(2)}` : 'No products in cart';
+const MiniCart = ({navigation}: any) => {
+  const ctx = useContext(CartContext)
+  const title = ctx.totalCount
+    ? `${ctx.totalItems} Products, total price $${ctx.totalAmount.toFixed(2)}`
+    : 'No products in cart'
 
-    return (
-        <View>
-            <Button onPress={() => (ctx.totalCount ? navigation.navigate('Cart') : false)} title={title} />
-        </View>
-    )
+  return (
+    <Button
+      onPress={() => (ctx.totalCount ? navigation.navigate('Cart') : false)}
+      title={title}
+    />
+  )
 }
 
-export default MiniCart;
+export default MiniCart
